eb706a68502fecd1be4cfaef910023318b903822
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-07-11 12:16:46 +0200

    task #02 - solution

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsElement = document.querySelector("#ingredients");
+const arrayLi = [];
+
+for (const ingredient of ingredients) {
+  const itemLi = document.createElement("li");
+  itemLi.innerText = ingredient;
+  itemLi.classList.add("item");
+  arrayLi.push(itemLi);
+}
+
+ingredientsElement.append(...arrayLi);

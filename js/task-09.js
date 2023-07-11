05d271e3a6f21199d17a8a74cf6eef36fd83a145
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-07-12 00:00:53 +0200

    task #09 - solution

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const obj = {
+  btn: document.querySelector("button.change-color"),
+  colorSpan: document.querySelector("span.color"),
+  body: document.body,
+};
+
+const changeBackground = () => {
+  const randomColor = getRandomHexColor();
+  obj.body.style.backgroundColor = randomColor;
+  obj.colorSpan.textContent = randomColor;
+}
+
+obj.btn.addEventListener("click", changeBackground);

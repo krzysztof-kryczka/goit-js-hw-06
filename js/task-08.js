fb1866847f74917dafb017a3b7f7ee3fea8162f4
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-07-11 23:28:55 +0200

    task #08 - solution

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const frmLogin = document.querySelector(".login-form");
 
+const submitForm = (e) => {
+  e.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled");
+  }
+
+  const frmUser = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(frmUser);
+  frmLogin.reset();
+};
+
+frmLogin.addEventListener("submit", submitForm);
